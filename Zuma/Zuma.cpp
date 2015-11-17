2abde1901dde8dcbc2e9266791b99fffdab80823
Revenge-Death <62452433hsm>
2015-11-17 17:54:24 +0800

    homework

--- Zuma/Zuma.cpp
+++ Zuma/Zuma.cpp
@@ -1,3 +1,144 @@
 #include <iostream>
 using namespace std;
+#include <string>
+#include "stdio.h"
 #include "list.h"
+#define _CRT_SECURE_NO_WARNINGS
+List<char> *list = new List<char>();
+void Print()
+{
+}
+
+void isClear(ListNodePosi(char) p)
+{
+	while (true)
+	{
+		ListNodePosi mid = p;
+		/*ListNodePosi mid = p;*/
+		ListNodePosi firstPi = new ListNode;
+		ListNodePosi lastPi = new ListNode;
+		int first = 0, last = 0;
+		if (_size < 3)
+			break;
+		while (true)
+		{
+			if (mid->pred != NULL && mid->Data == mid->pred->Data)
+
+			{
+				mid = mid->pred; first++;
+			}
+			else
+			{
+				firstPi = mid;
+				break;
+			}
+		}
+		while (true)
+		{
+			if (p->succ != NULL && p->Data == p->succ->Data)
+			{
+				p = p->succ; last++;
+			}
+			else
+			{
+				lastPi = p;
+				break;
+			}
+		}
+		if (last + first + 1 > 2)
+		{
+			int max = last + first + 1;
+			p = remove(firstPi, lastPi, max);
+		}
+		else
+			break;
+	}
+	return p;
+
+}
+
+void RePlay(ListNodePosi(char) po, long A, char B)
+{
+	ListNodePosi(char) p = list->find(A,list->size(),list->trailerNode);//找到插入位置
+	p = list->insertAsPred(p, B);//作为前驱插入
+    isClear(p);//在每次插入后判断是否需要消除
+	
+}
+int main()
+{
+	string seq;
+	int n;
+	int *A; char *B;
+	ListNodePosi(char) posi = new ListNode<char>;
+	getline(cin, seq);//输入字符串
+	int len = seq.size();
+	for (int i = 0; i < len; i++)
+		posi = list->insertAsSucc(list->headerNode, seq[i]);//一次插入后续节点值
+	scanf("%ld", &n);
+	A = new int[n];B = new char[n];
+	for (int i = 0; i < n; i++)
+		scanf("%ld %c", &A[i], &B[i]);
+	for (int i = 0; i < n; i++)
+		RePlay(posi, A[i], B[i]);
+	return 0;
+}
+
+//ListNodePosi List::isRemove(ListNodePosi p)
+//{
+//	while (true)
+//	{
+//		ListNodePosi mid = p;
+//		/*ListNodePosi mid = p;*/
+//		ListNodePosi firstPi = new ListNode;
+//		ListNodePosi lastPi = new ListNode;
+//		int first = 0, last = 0;
+//		if (_size < 3)
+//			break;
+//		while (true)
+//		{
+//			if (mid->pred != NULL && mid->Data == mid->pred->Data)
+//
+//			{
+//				mid = mid->pred; first++;
+//			}
+//			else
+//			{
+//				firstPi = mid;
+//				break;
+//			}
+//		}
+//		while (true)
+//		{
+//			if (p->succ != NULL && p->Data == p->succ->Data)
+//			{
+//				p = p->succ; last++;
+//			}
+//			else
+//			{
+//				lastPi = p;
+//				break;
+//			}
+//		}
+//		if (last + first + 1 > 2)
+//		{
+//			int max = last + first + 1;
+//			p = remove(firstPi, lastPi, max);
+//		}
+//		else
+//			break;
+//	}
+//	return p;
+//}  
+
+
+//ListNodePosi List::remove(ListNodePosi p, ListNodePosi pl,int count)
+//{
+//	/*char e = p->Data;*/
+//	ListNodePosi head = p->pred;
+//	pl->succ->pred = p->pred;
+//	p->pred->succ = pl->succ;
+//	/*delete p,  pl;*/
+//	_size -= count;
+//	return head;
+//}
+
--- Zuma/list.h
+++ Zuma/list.h
@@ -1,33 +1,44 @@
-#pragma once
 #include "iostream"
 using namespace std;
-typedef struct ListNode *ListNodePosi;
-struct ListNode {
-	int Data;
-	ListNodePosi pred;
-	ListNodePosi succ;
+
+#define ListNodePosi(T) ListNode<T>*
+template <typename T> struct ListNode {
+	T Data;
+	ListNodePosi(T) pred;
+	ListNodePosi(T) succ;
 };
-class List
+
+//template <typename T> struct ListNode {
+//	T Data;
+//	ListNodePosi pred;
+//	ListNodePosi succ;
+//};
+//typedef  ListNode<int>* ListNodePosi;//注意和上一个的区别
+
+template <typename T> class List //双向列表模板
 {
 private:
-	int _size; ListNodePosi header; ListNodePosi trailer;
+	int _size; ListNodePosi(T) header; ListNodePosi(T) trailer;//链表的大小，前驱，后继
 protected:
 	void init(); //列表创建时的初始化
 public:
 	List() { init(); }
 	~List() {}
-	ListNodePosi insertAsFirst(int e); //将e当作首节点插入
-	ListNodePosi insertAsLast(int e); //将e当作末节点插入
-	ListNodePosi insertAsSucc(ListNodePosi p, int e);
-	//ListNodePosi insertAsSucc( char e); //将e当作p的后继插入
-	ListNodePosi insertAsPred(ListNodePosi p, int e); //将e当作p的前驱插入
-													  /*ListNodePosi isRemove(ListNodePosi p);*/
-													  /*int remove(ListNodePosi p,ListNodePosi pt,int count);*/
-	int remove(ListNodePosi p);
-	ListNodePosi find(int count);
-	void Print();
-	bool isEmpty();
-	int GetSize();
-	ListNodePosi first();
+	//只读接口
+	ListNodePosi(T) find(T const& e, int n, ListNodePosi(T) p) const;
+	bool isEmpty() const { return _size <= 0; };
+	int size() const { return _size; }
+	ListNodePosi(T) headerNode(return header) const;
+	ListNodePosi(T) trailerNode(return trailer) const;
+	ListNodePosi(T) first() const;
+	ListNodePosi(T) last() const;
+
+	//可写接口
+	ListNodePosi(T) insertAsFirst(T const& e); //将e当作首节点插入
+	ListNodePosi(T) insertAsLast(T const& e); //将e当作末节点插入
+	ListNodePosi(T) insertAsSucc(ListNodePosi(T) p, T const& e);//将e当作p的后继插入
+	ListNodePosi(T) insertAsPred(ListNodePosi(T) p, T const& e); //将e当作p的前驱插入
+	T remove(ListNodePosi(T) p); //删除合法位置p处[将p作为参数传入前先保证其合法]的节点,返回被删除节点
+	
 };
 #pragma once
